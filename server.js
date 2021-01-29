1e00f92fb727230a38dffef29a270ced2d35bb52
John Diva <johndiva@switchmaven.com>
2021-01-29 16:38:14 +1000

    add 13 files for my adventure part2

--- server.js
+++ server.js
@@ -26,9 +26,31 @@ app.get(
 )
 
 app.get(
-    '/choice3'
+    '/choice1_1'
     ,function( request, response){
-    response.sendFile(path.join(__dirname, 'choice3.html'))
+    response.sendFile(path.join(__dirname, 'choice1_1.html'))
+}
+)
+
+app.get(
+    '/choice1_2'
+    ,function( request, response){
+    response.sendFile(path.join(__dirname, 'choice1_2.html'))
+}
+)
+
+
+app.get(
+    '/choice2_1'
+    ,function( request, response){
+    response.sendFile(path.join(__dirname, 'choice2_1.html'))
+}
+)
+
+app.get(
+    '/choice2_2'
+    ,function( request, response){
+    response.sendFile(path.join(__dirname, 'choice2_2.html'))
 }
 )
 
